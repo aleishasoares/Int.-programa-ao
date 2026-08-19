7332a5cc5228a1ddd1ce624c4216e8e89b3e1733
Aleisha Kauane Freire Soares <20261094010018@ifrn.local>
2026-08-19 14:10:44 -0300

    Quest. 8

--- q8.py
+++ q8.py
@@ -1,1 +1,21 @@
-print (" ola")
+continuar = "S"
+
+while continuar == "S":
+    n1 = float(input("Digite a primeira nota:"))
+
+    while n1 < 0 or n1 > 10:
+        print ("Nota invalida, digite um número entre 0 e 10")
+        n1 = float(input("Digite a primeira nota: "))
+    n2 = float(input(" Digite a segunda nota: "))
+
+    while n2 < 0 or n2 > 10:
+        print ("Nota invalida, digite um número entre 0 e 10")
+        n2 = float(input("Digite a segunda nota: "))
+
+    media = (n1+n2)/2
+    print ("A média aritmética das notas é:", media)
+
+    continuar = input("Deseja calcular a média de outro aluno? (S/N)").upper()
+    while continuar!= "S" and continuar != "N":
+        print (" Opçao invalida, digite S ou N!")
+        continuar = input("NOVO CALCULO S/N: ")
