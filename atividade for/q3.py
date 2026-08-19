92b3c0f52ade233c9a63006b576b055c9bb9ec45
Aleisha Kauane Freire Soares <20261094010018@ifrn.local>
2026-08-19 15:08:11 -0300

    Laços for

---
+++ atividade for/q3.py
@@ -0,0 +1,7 @@
+soma = 0
+
+for n in range (10):
+    n =  int(input("Digite um número:"))
+    soma = soma + n
+
+print ("A soma do número é: ", soma)
---
+++ atividade for/q4.py
@@ -0,0 +1,8 @@
+soma = 0
+
+for n in range (10):
+    n = int(input("Digite um número: "))
+    divisao = n/2
+    soma = soma + divisao 
+
+print ("A soma do número é: ", soma)
